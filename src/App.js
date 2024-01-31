ae0ef4d71ad5cb4f1f297083d32df0577a4dc493
filipNajdovski <filip-najdovski@live.com>
2024-01-31 14:20:17 +0100

    added Header and Footer Layout + updated readme file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>Content</main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import footerSvg from '../assets/footer.svg'
+
+const footerYear = new Date().getFullYear()
+
+
+function Footer() {
+  return (
+    <footer className='footer p-10 bg-grey-700 text-primary-content footer-center'>
+        <img src={footerSvg} alt="footer svg" />
+        <p>Copyright &copy; {footerYear}</p>
+    </footer>
+  )
+}
+
+export default Footer
