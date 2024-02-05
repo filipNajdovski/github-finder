a748345144cc786c083cbfe48450c7d02bd2d9bf
filipNajdovski <filip-najdovski@live.com>
2024-02-05 13:49:57 +0100

    switched useState to useReducer Hook

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <>
+      {/* SEARCH COMPONENTS WILL GO HERE */}
+        <UserResults />
+    </>
+  )
+}
+
+export default Home
